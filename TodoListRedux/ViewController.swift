035629769b5a625e592497e893f1b7535dfc32eb
Kiattisak Anoochitarom <macbaszii@gmail.com>
2016-12-01 21:11:36 +0700

    Initial Commit

---
+++ TodoListRedux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoListRedux
+//
+//  Created by Kiattisak Anoochitarom on 12/1/2559 BE.
+//  Copyright © 2559 Hexter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
